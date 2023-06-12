8cb9b5689fba2916a6c235dde390307dbf4d07d1
Alexander Lee Smith <63920477+lapsangsouchy@users.noreply.github.com>
2023-06-12 16:52:36 -0400

    3D Model added and polish of Items Panel

--- src/Panels/Items/Items.tsx
+++ src/Panels/Items/Items.tsx
@@ -6,35 +6,34 @@ const Items = () => {
     <div id='items-panel'>
       <div id='top-bar'>
         <button className='selector'>L</button>
-        <div className='categories'>
-          <button className='menu-select'>
-            <img src='src/assets/icons/TotK_Armor_Icon.png' alt='' />
-          </button>
-          <button className='menu-select'>
-            <img src='src/assets/icons/TotK_Bows_and_Arrows_Icon.png' alt='' />
-          </button>
-          <button className='menu-select'>
-            <img src='src/assets/icons/TotK_Shields_Icon.png' alt='' />
-          </button>
-          <button className='menu-select'>
-            <img src='src/assets/icons/TotK_Weapons_Icon.png' alt='' />
-          </button>
-          <button className='menu-select'>
-            <img src='src/assets/icons/TotK_Material_Icon.png' alt='' />
-          </button>
-          <button className='menu-select'>
-            <img src='src/assets/icons/TotK_Food_Icon.png' alt='' />
-          </button>
-          <button className='menu-select'>
-            <img src='src/assets/icons/TotK_Zonai_Device_Icon.png' alt='' />
-          </button>
-          <button className='menu-select'>
-            <img src='src/assets/icons/TotK_Key_Items_Icon.png' alt='' />
-          </button>
-          <button className='menu-select'>
-            <img src='src/assets/icons/settings-icon.png' alt='' />
-          </button>
-        </div>
+
+        <button className='menu-select'>
+          <img src='src/assets/icons/TotK_Armor_Icon.png' alt='' />
+        </button>
+        <button className='menu-select'>
+          <img src='src/assets/icons/TotK_Bows_and_Arrows_Icon.png' alt='' />
+        </button>
+        <button className='menu-select'>
+          <img src='src/assets/icons/TotK_Shields_Icon.png' alt='' />
+        </button>
+        <button className='menu-select'>
+          <img src='src/assets/icons/TotK_Weapons_Icon.png' alt='' />
+        </button>
+        <button className='menu-select'>
+          <img src='src/assets/icons/TotK_Material_Icon.png' alt='' />
+        </button>
+        <button className='menu-select'>
+          <img src='src/assets/icons/TotK_Food_Icon.png' alt='' />
+        </button>
+        <button className='menu-select'>
+          <img src='src/assets/icons/TotK_Zonai_Device_Icon.png' alt='' />
+        </button>
+        <button className='menu-select'>
+          <img src='src/assets/icons/TotK_Key_Items_Icon.png' alt='' />
+        </button>
+        <button className='menu-select' id='settings-btn'>
+          <img src='src/assets/icons/settings-icon.png' alt='' />
+        </button>
 
         <button className='selector'>R</button>
       </div>
